579bfc0f52f4166a998de6e671b7a3eeb44d550f
Brian Mazini Siervi <brianmazini@gmail.com>
2020-01-15 22:13:24 -0300

    salvando um contato no bd

---
+++ app06/lib/ui/home_page.dart
@@ -0,0 +1,33 @@
+import 'package:app06/helpers/contact_helper.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  ContactHelper helper = ContactHelper();
+
+  @override
+  void initState() {
+    super.initState();
+
+    Contact c = Contact();
+    c.name = "joao";
+    c.email = "joao@gmail.com";
+    c.phone = "+5511999991234";
+    c.img = "imgtest";
+
+    helper.saveContact(c);
+
+    helper.getAllContacts().then((list) {
+      print(list);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
