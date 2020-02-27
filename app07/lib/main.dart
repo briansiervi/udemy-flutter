078b42a36dab7344da0e9e44590e835c6f2368e0
Brian Mazini Siervi <brianmazini@gmail.com>
2020-02-26 21:38:12 -0300

    App07 - commit inicial

--- app07/lib/main.dart
+++ app07/lib/main.dart
@@ -1,21 +1,37 @@
+import 'package:app07/chat_screen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-void main(){  
+void main() async{
   runApp(MyApp());
 
-  Firestore.instance.collection("col").document("doc").setData({"texto":"brian"});
+  // Firestore.instance.collection("mensagens").document("msg1").snapshots().listen((event) {
+  //   print(event.data);
+  // });
+
+  // Firestore.instance.collection("mensagens").snapshots().listen((event) {
+  //   event.documents.forEach((element) {
+  //     print(element.data);
+  //   });
+  // });  
+
+  // QuerySnapshot snapshot = await Firestore.instance.collection("mensagens").getDocuments();
+  // snapshot.documents.forEach((d){
+  //   d.reference.updateData({"lido": false});
+  //   print(d.data);
+  //   print(d.documentID);
+  // });
 }
 
-class MyApp extends StatelessWidget {  
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(        
+      theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Container(),
+      home: ChatScreen(),
     );
   }
 }
